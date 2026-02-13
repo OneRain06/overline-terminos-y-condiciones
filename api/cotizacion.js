da3b2ed6f9361977eb068f25d92432d0849944a9
OneRain06 <139792954+OneRain06@users.noreply.github.com>
2026-02-13 16:11:06 -0500

    Change Content-Type to application/json in API request

--- api/cotizacion.js
+++ api/cotizacion.js
@@ -11,9 +11,9 @@ export default async function handler(req, res) {
       {
         method: "POST",
         headers: {
-          "Content-Type": "text/plain"
-        },
-        body: JSON.stringify(req.body)
+  "Content-Type": "application/json"
+},
+body: JSON.stringify(req.body)
       }
     );
 
